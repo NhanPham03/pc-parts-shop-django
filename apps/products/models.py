eb684a7284984d8840822cd0ba9446b6af6d8fde
NhanPham03 <ph.nhan03@gmail.com>
2024-06-11 09:52:21 +0700

    ProductTypes Enums > TextChoices

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,5 +1,15 @@
 from django.db import models
-from apps.products.enums import ProductTypes
+from django.utils.translation import gettext_lazy as _
+
+class ProductTypes(models.TextChoices):
+  CPU = 'CPU', _('CPU')
+  GPU = 'GPU', _('GPU')
+  RAM = 'RAM', _('RAM')
+  PSU = 'PSU', _('PSU')
+  STORAGE = 'STORAGE', _('Storage')
+  MOTHERBOARD = 'MOTHERBOARD', _('Motherboard')
+  CASE = 'CASE', _('Case')
+  OTHER = 'OTHER', _('Other')
 
 class Product(models.Model):
   description = models.TextField(default='')
